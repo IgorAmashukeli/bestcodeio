353e6ee0cef7cde29e302b50b345b5ff4decd2f6
IgorAmashukeli <amashukeliim1150@gmail.com>
2024-03-07 14:20:19 +0000

    Renamed local repository directory

--- AngProject/src/app/app.config.ts
+++ AngProject/src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes, withViewTransitions()),
+    provideAnimations(),
+  ],
 };
---
+++ AngProject/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyDzqjv2xuZbdRrgUarTbm026l7ngVjAbHw',
+    authDomain: 'bestcode-2df22.firebaseapp.com',
+    projectId: 'bestcode-2df22',
+    storageBucket: 'bestcode-2df22.appspot.com',
+    messagingSenderId: '593704260118',
+    appId: '1:593704260118:web:2de25502e20f53aa319b37',
+    measurementId: 'G-YGRSWY4KLM',
+  },
+};
